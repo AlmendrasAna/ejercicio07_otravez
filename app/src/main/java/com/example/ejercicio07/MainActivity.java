e1130add73703b8935eb0f3cd3e080b97c73f4a3
Ana <almendras451@gmail.com>
2023-06-28 15:01:48 -0400

    commit tostadas listas falta el switch

--- app/src/main/java/com/example/ejercicio07/MainActivity.java
+++ app/src/main/java/com/example/ejercicio07/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.ejercicio07;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
 import android.widget.TextClock;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextClock reloj = findViewById(R.id.textClock2);
+        Switch switchNight = findViewById(R.id.switchTheme);
+        Toast.makeText(getBaseContext(), "Bienvenido (/o____o)/", Toast.LENGTH_LONG).show();
+
+        switchNight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (switchNight.isChecked()) {
+                    
+
+                    Toast.makeText(getBaseContext(), "Enable Dark Mode", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    Toast.makeText(getBaseContext(), "Enable Light Mode", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
+
 }
